22ec491e5d0311a843ec733e14b175e594823c94
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-31 17:13:40 +0900

    initial commit

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from .models import Tag , Task
+from rest_framework import viewsets
+from .serializers import TagSerializer, TaskSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
